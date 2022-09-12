b6c56b9add239a4147cac81a4ac0cd9dccf8d4ca
Jordy <j.j.c.van.raalte@gmail.com>
2022-09-12 15:52:04 +0200

    Fixed naming error

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -5,7 +5,7 @@ export default class User {
 
     constructor(id: string, username: string, avatar: string) {
         this.id = id;
-        this.avatar = avatar;
         this.username = username;
+        this.avatar = avatar;
     }
 }
